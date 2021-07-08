110f92ef7744308139c233d63a06096eb626b633
ffajar.pratama <pfajarfa14.9b@gmail.com>
2021-07-08 18:47:33 +0700

    modify / route

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('auth.login');
+    return redirect()->route('login');
 });
 
 Route::get('/location-test', function () {
